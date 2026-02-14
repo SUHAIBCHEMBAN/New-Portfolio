e7a8d83c7db8e98b8ba204bcaf8e5c426e3e50da
SUHAIBCHEMBAN <shuhaibchemban2004@gmail.com>
2026-02-14 16:40:12 +0530

    update:updated resume

--- src/data/personal.js
+++ src/data/personal.js
@@ -20,7 +20,7 @@ export const personalInfo = {
     github: "https://github.com/SUHAIBCHEMBAN",
     linkedin: "https://www.linkedin.com/in/suhaib-c-114b10292/",
   },
-  resumeUrl: "/assets/suhaibsresume.pdf"
+  resumeUrl: "/assets/Suhaibsresume.pdf"
 };
 
 export const experience = [
